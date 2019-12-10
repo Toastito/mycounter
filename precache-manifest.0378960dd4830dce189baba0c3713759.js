76a1f2e47748c713d5aa4c645af2c0a96ef3b9f4
Roger <rogerddavila@gmail.com>
2019-12-10 00:17:22 -0800

    Updates

---
+++ precache-manifest.0378960dd4830dce189baba0c3713759.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c296933ef76195afc76cbe01029ae2ad",
+    "url": "/mycounter/index.html"
+  },
+  {
+    "revision": "74be2c77545e07e64950",
+    "url": "/mycounter/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "835937de3c93be1bdd90",
+    "url": "/mycounter/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "74be2c77545e07e64950",
+    "url": "/mycounter/static/js/2.38eb983d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mycounter/static/js/2.38eb983d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "835937de3c93be1bdd90",
+    "url": "/mycounter/static/js/main.b17bfc00.chunk.js"
+  },
+  {
+    "revision": "6c6319ccc8e160db50a9",
+    "url": "/mycounter/static/js/runtime-main.afc58827.js"
+  }
+]);
